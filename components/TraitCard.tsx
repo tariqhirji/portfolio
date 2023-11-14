613be0c6f4f9b88cfac5e044fe630da677b99f2a
Tariq Hirji <tariqhirji15@gmail.com>
2023-11-14 11:18:27 -0500

    traitcard images

--- components/TraitCard.tsx
+++ components/TraitCard.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import TEAMPLAYER from "@/public/team_Player.svg";
+import INNOVATIVE from "@/public/innovative.svg";
+import COMMUNICATOR from "@/public/communicator.svg";
 
 interface TraitCardProps {
   title: string;
@@ -9,7 +11,15 @@ interface TraitCardProps {
 const TraitCard: React.FC<TraitCardProps> = ({ title, description }) => {
   return (
     <div className="card card_secondary flex flex-col gap-4 items-center">
-      <Image src={TEAMPLAYER} alt="Team Player" width={200} height={200} />
+      {title === "Team Player" && (
+        <Image src={TEAMPLAYER} alt={title} width={200} height={200} />
+      )}
+      {title === "Innovative" && (
+        <Image src={INNOVATIVE} alt={title} width={200} height={200} />
+      )}
+      {title === "Communicator" && (
+        <Image src={COMMUNICATOR} alt={title} width={200} height={200} />
+      )}
       <h2 className="font-bold">{title}</h2>
       <p className="px-8 text-center">{description}</p>
     </div>
