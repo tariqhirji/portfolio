4c4758519ea09cf39f80796bf93d3bc571689020
Tariq Hirji <tariqhirji15@gmail.com>
2023-11-17 15:49:29 -0500

    attemped api of emailjs and football-api

--- components/SportsDynamic.tsx
+++ components/SportsDynamic.tsx
@@ -1,4 +1,19 @@
+"use client";
+import { useEffect } from "react";
+import axios from "axios";
 const SportsDynamic = () => {
+  const config = {
+    headers: {
+      "X-Auth-Token": `${process.env.NEXT_PUBLIC_SPORT}`,
+    },
+  };
+  useEffect(() => {
+    axios
+      .get("http://api.football-data.org/v4/competitions/2021/teams", config)
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e));
+  });
+
   return (
     <div className="card card_secondary w-full flex items-center justify-center">
       <p>My favourite team&apos;s next game:</p>
