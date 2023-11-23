e92680ceedb205d0e9dc66846546b09a3056a417
Tariq Hirji <tariqhirji15@gmail.com>
2023-11-23 16:16:40 -0500

    Pause navbar and axios sport request for debugging

--- components/SportsDynamic.tsx
+++ components/SportsDynamic.tsx
@@ -2,21 +2,23 @@
 import { useEffect } from "react";
 import axios from "axios";
 const SportsDynamic = () => {
-  const config = {
-    headers: {
-      "X-Auth-Token": `${process.env.NEXT_PUBLIC_SPORT}`,
-    },
-  };
-  useEffect(() => {
-    axios
-      .get("http://api.football-data.org/v4/competitions/2021/teams", config)
-      .then((res) => console.log(res))
-      .catch((e) => console.log(e));
-  });
+  // useEffect(() => {
+  //   const config = {
+  //     headers: {
+  //       "X-Auth-Token": `${process.env.NEXT_PUBLIC_SPORT}`,
+  //     },
+  //   };
+  //   axios
+  //     .get("https://api.football-data.org/v4/competitions/2021/teams", config)
+  //     .then((res) => console.log(res))
+  //     .catch((e) => console.log(e));
+  // }, []);
 
   return (
     <div className="card card_secondary w-full flex items-center justify-center dark:text-secondary">
-      <p className="font-bold text-2xl font-shareTech">My favourite team&apos;s next game:</p>
+      <p className="font-bold text-2xl font-shareTech">
+        My favourite team&apos;s next game:
+      </p>
     </div>
   );
 };
