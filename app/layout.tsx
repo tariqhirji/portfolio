2ff1890fb824ea49323f56298323dbdbc4a3da5c
Tariq Hirji <tariqhirji15@gmail.com>
2024-01-15 10:24:31 -0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={"font-firaSans"}>
         <main>{children}</main>
+        <Analytics />
       </body>
     </html>
   );
