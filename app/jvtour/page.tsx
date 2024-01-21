b75a040264de96189eddd424d65c9006ecd4f476
Tariq Hirji <tariqhirji15@gmail.com>
2024-01-21 12:37:01 -0500

    update jv designs

--- app/jvtour/page.tsx
+++ app/jvtour/page.tsx
@@ -1,12 +1,12 @@
-import JV from "../../public/jv.jpg";
+import JVTour from "../../public/jvtour.jpg";
 import Image from "next/image";
 
 export default function Page() {
   return (
     <div className="jv">
       <Image
-        src={JV}
-        alt="JV Designs"
+        src={JVTour}
+        alt="JV Designs - Tour"
         width={0}
         height={0}
         style={{ width: "100%", height: "100%" }}
